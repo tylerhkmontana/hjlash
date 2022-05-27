d01212a2f99965fb80073ad7602689b3932ded1a
Hyunbin Tyler Kim <tylerhkmontana@gmail.com>
2022-05-27 02:32:12 -0400

    update upto location

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import Nav from '../components/nav'
 import Logo from '../components/logo'
 import Carousels from '../components/carousels'
+import Footer from '../components/footer'
 import fs from 'fs'
 import path from 'path'
 
@@ -41,13 +42,35 @@ export default function Home(files) {
 
             <div className={styles.about_container}>
                 <div className={styles.carousels_container}>
-                    <Carousels width={400} height={500} urls={files}/>
+                    <Carousels width={600} height={500} urls={files}/>
                 </div>
 
-                <div>
-                    <p className={styles.about_content}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                <div className={styles.about_content}>
+                    {/* Put informations about facilities, staffs, experiences, provided services */}
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
                 </div>
             </div>
+
+            <h1 className={styles.section_divider}>Pricing</h1>
+
+            <div className={styles.pricing_container}>
+                <div className={styles.menu_container}>
+                    <Image layout='fill' src='/menu.jpg' />
+                </div>
+            </div>
+
+            <h1 className={styles.section_divider}>Location</h1>
+
+            <div className={styles.location_container}>
+                <div className={styles.map_container}>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3017.1121277807347!2d-74.07239528434107!3d40.86941343590682!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2f9993bab000d%3A0x538bd772c79f07ef!2s440%20Boulevard%2C%20Hasbrouck%20Heights%2C%20NJ%2007604!5e0!3m2!1sen!2sus!4v1653630003040!5m2!1sen!2sus" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div className={styles.location_content}>
+                    <p>440 Boulevard, Hasbrouck Heights, NJ 07604</p>
+                </div>  
+            </div>
+
+            <Footer />
         </div>
     )
 }
